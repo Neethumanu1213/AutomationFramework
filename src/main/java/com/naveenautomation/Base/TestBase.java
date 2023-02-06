c98f65191d8a78b8643e294d5b03c792ae64695d
neethumanu1213 <neethumanu1231@gmail.com>
2023-02-06 15:43:22 -0500

    updated TestBase class

--- src/main/java/com/naveenautomation/Base/TestBase.java
+++ src/main/java/com/naveenautomation/Base/TestBase.java
@@ -14,19 +14,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestBase {
 
 	public static WebDriver driver;
-	public Browsers DEFAULT_BROWSER = Browsers.GOOGLE_CHROME;
+	//public Browsers DEFAULT_BROWSER = Browsers.GOOGLE_CHROME;
+	public String DEFAULT_BROWSER = getBrowser("browser");
 	public static WebdriverEvents events = new WebdriverEvents();
 	public EventFiringWebDriver eventFiringWebDriver;
 
 	public void launchBrowser() {
 
 		switch (DEFAULT_BROWSER) {
-		case GOOGLE_CHROME:
+		//case GOOGLE_CHROME:
+		case "chrome":
 
 			driver = new ProxyDriver(WebDriverManager.chromedriver().create());
 			break;
 
-		case EDGE:
+		//case EDGE:
+		case "edge":
 			WebDriverManager.edgedriver().setup();
 			driver = new ProxyDriver(WebDriverManager.edgedriver().create());
 			break;
@@ -51,6 +54,18 @@ public class TestBase {
 
 	public void quitBrowser() {
 		driver.quit();
+
+	}
+
+	private String getBrowser(String name) {
+		String value = System.getProperty(name);
+		if (value == null) {
+			throw new RuntimeException(name + " is not a parameter");
+		} else if (value.isEmpty()) {
+			throw new RuntimeException(name + " is empty");
+		}
+
+		return value;
 
 	}
 
